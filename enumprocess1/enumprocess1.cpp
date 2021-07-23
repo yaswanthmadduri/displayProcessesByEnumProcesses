2dc3484cad9d658fe266d558e634ce63ca600b8e
yaswanthmadduri <83799086+yaswanthmadduri@users.noreply.github.com>
2021-07-23 10:50:16 +0530

    final commit

--- enumprocess1/enumprocess1.cpp
+++ enumprocess1/enumprocess1.cpp
@@ -11,63 +11,59 @@ void processDetails(DWORD dwprocessID);
 
 
 int main() {
-
-	//DWORD processIdentifiers[100] = { 0 };
 	
-	DWORD lengthOfArray = 300;//in bytes
-	DWORD* processIdentifiers;
-	processIdentifiers = (DWORD*)malloc(lengthOfArray * sizeof(DWORD));//holds address of first place in the dynamically created array
-	if (processIdentifiers == NULL)
+	DWORD proc_array_len = 400;//in bytes (each dword is of 4 bytes)
+	DWORD* ptr_proc_identifiers;// pointer to proc_identifiers
+	ptr_proc_identifiers = (DWORD*)malloc(proc_array_len * sizeof(DWORD));//holds address of first place in the dynamically created array
+	if (ptr_proc_identifiers == NULL)
 	{
 		cout << "Memory Allocation Failed";
 		exit(1);
 	}
 	else {
-		cout << "Memory is now allocated" << endl;
+		cout << "Memory is now allocated to store " << proc_array_len / sizeof(DWORD) << " process identifiers." << endl;
 	}
 
-	cout << "process identifiers allocated : " << lengthOfArray / sizeof(DWORD)  << endl;
-
 	DWORD bytesNeededForStoring = 0;
-	BOOL bEnumProcess = EnumProcesses(processIdentifiers, lengthOfArray, &bytesNeededForStoring);
+	BOOL bEnumProcess = EnumProcesses(ptr_proc_identifiers, proc_array_len, &bytesNeededForStoring);
 	if (bEnumProcess == FALSE) {
 		cout << "ENUM Process identifier fetching function failed " << endl << endl;
 		cout << "Error code: " << GetLastError() << endl;
 		return 0;
 	}
-	cout << "ENUM Process identifier fetching function succeeded" << endl << endl;
+	//cout << "ENUMProcesses function fetched" << proc_array_len / sizeof(DWORD) << "process identifiers" << endl << endl;
 
 	//when bytesneededforstoring == length of process identifiers array
 
-	while (bytesNeededForStoring == lengthOfArray) { 
+	while (bytesNeededForStoring == proc_array_len) {
 
-		lengthOfArray = lengthOfArray + 100;
-		processIdentifiers = (DWORD*)realloc(processIdentifiers, lengthOfArray * sizeof(DWORD));
-		if (processIdentifiers == NULL)
+		proc_array_len = proc_array_len + 100;// 100 bytes means 25 new processes can be stored (size of each dword is 4 bytes)
+		ptr_proc_identifiers = (DWORD*)realloc(ptr_proc_identifiers, proc_array_len * sizeof(DWORD));
+		if (ptr_proc_identifiers == NULL)
 		{
-			cout << "Memory ReAllocation Failed";
+			cout << "Memory Re-allocation failed";
 			exit(1);
 		}
 		else {
-			cout << "Memory is now re-allocated" << endl;
+			cout << "Memory is now re-allocated to store " << proc_array_len / sizeof(DWORD) << " process identifiers." << endl;
 		}
 
-		BOOL bEnumProcess = EnumProcesses(processIdentifiers, lengthOfArray, &bytesNeededForStoring);
-		cout << lengthOfArray / sizeof(DWORD) << " : These many processes can fit in the process identifier array now" << endl; 
+		BOOL bEnumProcess = EnumProcesses(ptr_proc_identifiers, proc_array_len, &bytesNeededForStoring);
 
 	}
 
 	DWORD totalNoOfProcesses = bytesNeededForStoring / sizeof(DWORD);
 
-	for (DWORD l_proc_index = 0; l_proc_index < totalNoOfProcesses; l_proc_index++) {
+	for (DWORD l_proc_index = 1; l_proc_index <= totalNoOfProcesses; l_proc_index++) {
 
-		processDetails(processIdentifiers[l_proc_index]);
+		cout << "Process " << l_proc_index << ": " << endl;
+		processDetails(ptr_proc_identifiers[l_proc_index]);
 
 	}
 
 	cout << "Total number of processes : " << totalNoOfProcesses << endl;
 
-	free(processIdentifiers);
+	free(ptr_proc_identifiers);
 
 	return 0;
 }
@@ -92,7 +88,7 @@ void processDetails(DWORD dwprocessID) {
 
 	}
 
-	_tprintf(TEXT("Module base name : %s \n"), ProcessName);
+	_tprintf(TEXT("Process name : %s \n"), ProcessName);
 	_tprintf(TEXT("Process path : %s \n"), ProcessPath);
 	wcout << "Process identifier : " << dwprocessID << endl << endl;
 	cout << "---------------------------------------" << endl;
@@ -104,15 +100,3 @@ void processDetails(DWORD dwprocessID) {
 	}
 
 }
-
-/*
-
-if (y == bytesNeededForStoring) {
-		//we dont know if processes still exist
-		int	x = sizeof(processIdentifiersInitially) + 1;
-		cout << "sizeof(processIdentifiers)" << sizeof(processIdentifiersInitially) << endl;
-		cout << "x=" << x << endl;
-	}
-
-
-*/
